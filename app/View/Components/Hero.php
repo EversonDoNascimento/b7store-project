3b6fd64af009cd8777c840dc2a05d4b7fa98f9d8
EversonDoNascimento <everson25nascimento@gmail.com>
2025-03-10 09:56:07 -0300

    feat: Created filter in home page

--- app/View/Components/Hero.php
+++ app/View/Components/Hero.php
@@ -2,6 +2,8 @@
 
 namespace App\View\Components;
 
+use App\Services\CategoryService;
+use App\Services\StateService;
 use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
@@ -16,21 +18,8 @@ class Hero extends Component
     public function __construct()
     {
      
-        $this->states = [
-            ['value' => 'SP', 'name' => 'São Paulo'],
-            ['value' => 'PE', 'name' => 'Pernambuco'],
-            ['value' => 'MG', 'name' => 'Minas Gerais'],
-            ['value' => 'PB', 'name' => 'Paraíba'],
-            ['value' => 'RJ', 'name' => 'Rio de Janeiro'],
-            ['value' => 'MS', 'name' => 'Mato Grosso do Sul']
-        ];
-        $this->categories = [
-            ['value' => 'car', 'category' => 'SW4'],
-            ['value' => 'house', 'category' => 'Glass'],
-            ['value' => 'Technology', 'category' => 'Computer'],
-            ['value' => 'clothes', 'category' => 'clothes'],
-            ['value' => 'babies', 'category' => 'babies']
-        ];
+        $this->states = StateService::getAllStates();
+        $this->categories = CategoryService::getAllCategories();
     }
 
     /**
--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,25 +1,29 @@
 <section class="hero-area">
     <h3 class="subtitle">Aqui você encontra o que tanto procura!</h3>
     <h1 class="title">O que você está procurando?</h1>
-    <div class="search-area">
-        <input
-        class="search-text"
-        type="text"
-        placeholder="Estou procurando por..."
-        />
-        <select class="categories-options">
-            <option selected hidden disabled value="">Categoria</option>
+    <form style="width: 100%;" method="GET" action="{{route('ad.list')}}">
+        <div class="search-area">
+            <input
+            class="search-text"
+            type="text"
+            name="s"
+            placeholder="Estou procurando por..."
+            />
+            <select name="c" class="categories-options">
+                <option selected hidden disabled value="">Categoria</option>
 
-            @forEach($categories as $c)
-                <option value="{{$c['value']}}">{{$c['category']}}</option>
-            @endforEach
-        </select>
-        <select class="states">
-            <option selected hidden disabled value="">Estado</option>
-            @forEach($states as $state)
-                <option value="{{$state['value']}}">{{$state['name']}}</option>
-            @endforEach
-        </select>
-        <button class="search-button">Procurar</button>
-    </div>
+                @forEach($categories as $c)
+                    <option value="{{$c['id']}}">{{$c['name']}}</option>
+                @endforEach
+            </select>
+            <select name="st" class="states">
+                <option selected hidden disabled value="">Estado</option>
+                @forEach($states as $state)
+                    <option value="{{$state['id']}}">{{$state['name']}}</option>
+                @endforEach
+            </select>
+            <button class="search-button" type="submit">Procurar</button>
+        </div>
+    </form>
+
 </section>
